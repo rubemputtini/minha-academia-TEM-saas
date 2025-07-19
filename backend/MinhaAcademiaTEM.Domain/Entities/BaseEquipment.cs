3348de407c482997aa127d21ab69737f2be6d647
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-07-19 15:50:17 +0100

    chore: create base equipment for customization

--- backend/MinhaAcademiaTEM.Domain/Entities/BaseEquipment.cs
+++ backend/MinhaAcademiaTEM.Domain/Entities/BaseEquipment.cs
@@ -1,12 +1,12 @@
 namespace MinhaAcademiaTEM.Domain.Entities;
 
-public class Equipment : CoachEntity
+public class BaseEquipment : BaseEntity
 {
     public string Name { get; set; } = string.Empty;
+    public string PhotoUrl { get; set; } = string.Empty;
     public string VideoUrl { get; set; } = string.Empty;
 
     public MuscleGroup MuscleGroup { get; set; }
 
-    public Guid BaseEquipmentId { get; set; }
-    public BaseEquipment BaseEquipment { get; set; } = null!;
+    public ICollection<Equipment> Customizations { get; set; } = new List<Equipment>();
 }

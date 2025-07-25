d722b6db3c9a370117c1a420e96142f077a1fd26
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-07-25 18:14:01 +0100

    chore: add new user repository methods

--- backend/MinhaAcademiaTEM.Domain/Interfaces/IUserRepository.cs
+++ backend/MinhaAcademiaTEM.Domain/Interfaces/IUserRepository.cs
@@ -7,8 +7,13 @@ public interface IUserRepository
     Task<User?> GetByIdAsync(Guid id);
     Task<User?> GetByEmailAsync(string email);
     Task<List<User>> GetAllAsync();
+    Task<int> GetTotalUsersAsync();
     Task<List<User>> GetAllByCoachIdAsync(Guid coachId);
     Task AddAsync(User user);
     Task UpdateAsync(User user);
     Task DeleteAsync(User user);
+    Task<List<User>> SearchAsync(string? search, int skip, int take);
+    Task<int> CountAsync(string? search);
+    Task<int> CountClientsByCoachIdAsync(Guid coachId);
+    Task<Dictionary<Guid, int>> GetClientsCountForCoachesAsync(List<Guid> coachIds);
 }

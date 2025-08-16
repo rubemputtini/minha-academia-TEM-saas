c1b0613f232d82e7c1ba6f2fbd6ba4d7d77af1e9
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-08-16 13:27:34 +0100

    refactor: modify restrictions to smtp configuration

--- backend/MinhaAcademiaTEM.Domain/Configuration/SmtpConfiguration.cs
+++ backend/MinhaAcademiaTEM.Domain/Configuration/SmtpConfiguration.cs
@@ -1,9 +1,9 @@
 namespace MinhaAcademiaTEM.Domain.Configuration;
 
-public class SmtpConfiguration
+public sealed class SmtpConfiguration
 {
-    public string Host { get; set; } = string.Empty;
-    public int Port { get; set; }
-    public string UserName { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
+    public string Host { get; init; } = string.Empty;
+    public int Port { get; init; }
+    public string UserName { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
 }

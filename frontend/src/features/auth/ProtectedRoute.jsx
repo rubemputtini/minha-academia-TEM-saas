92c8d09f0e61f74ecfe363b566809673f5a7e67c
Rubem Staciarini Puttini <rubem.staciariniputtini@gmail.com>
2025-09-23 19:03:56 +0100

    bug: improve protected route to use previous location and correct role

--- frontend/src/features/auth/ProtectedRoute.jsx
+++ frontend/src/features/auth/ProtectedRoute.jsx
@@ -1,22 +1,23 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import { ROUTES } from "@/shared/routes/routes";
 import LoadingCard from "@/shared/ui/LoadingCard";
 import { useAuth } from "./hooks/useAuth";
 
 export default function ProtectedRoute({ children, publicRoute = false, requireAdmin = false }) {
     const { isAuthenticated, role, loading } = useAuth();
+    const location = useLocation();
 
     if (loading)
-        return <LoadingCard fullScreen size={40} />
+        return <LoadingCard fullScreen size={40} />;
 
     if (publicRoute)
         return children;
 
     if (!isAuthenticated)
-        return <Navigate to={ROUTES.login} replace />;
+        return <Navigate to={ROUTES.login} replace state={{ from: location }} />;
 
-    if (requireAdmin && role !== "admin")
-        return <Navigate to={ROUTES.dashboard} replace />;
+    if (requireAdmin && role !== "Admin")
+        return <Navigate to={ROUTES.account} replace />;
 
     return children;
 }
